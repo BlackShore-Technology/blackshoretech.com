74920e058ec11a8981eb7cc1e7a34aeffce52408
brsc2909 <brsc2909@users.noreply.github.com>
2022-09-01 13:37:22 +0000

    deploy: 0f6578ddd38c8b1d94af6f9b9afe44df53c4a73b

--- _nuxt/useLang.157c0804.js
+++
@@ -1,1 +0,0 @@
-import{E as t}from"./entry.692d8551.js";const o=()=>{const{t:s}=t();return{t:s}};export{o as u};
---
+++ _nuxt/useLang.76a9592f.js
@@ -0,0 +1,1 @@
+import{E as t}from"./entry.2f46f950.js";const o=()=>{const{t:s}=t();return{t:s}};export{o as u};
